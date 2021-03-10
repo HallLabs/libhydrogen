01dee7f2bc5b99509d6375847d6c2cb45a3166e7
Alec Raymond <alec.raymond@medic.life>
2021-03-10 13:13:02 -0700

    moved stm true random generator function into libhydrogen repo for easier tracking

--- impl/random/stm32.h
+++ impl/random/stm32.h
@@ -1,5 +1,38 @@
 /* Interface with STM32 model microcontroller. Implements stm_true_random() */
-#include "hydrogen_system_stm32.h"
+#include "adc.h"
+
+/**
+ * @Brief    Function to return a true random number source from STM32 microcontrollers.
+ * @Note     IMPORTANT:   pre-release implementation for generation of small batches of unique numbers.
+ *                        Not yet random. Do not use for security tokens or encryption.
+ * @Note     ADC1 internal temperature sensor must be enabled with default settings.
+ * @todo     optimize timing; added extra delays all over while debugging adc timeout
+ */
+
+static inline uint32_t stm_true_random(void)
+{
+    uint32_t temp_entropy = 0;
+    for (uint8_t position = 0; position < 32; position += 2)            // acquire 32 bits of entropy
+    {
+        // Gather random bits from temperature sensor.  TODO: 3rd order difference algorithm to improve randomness.
+        HAL_ADC_Start(&hadc1);
+        HAL_Delay(10);
+        while(HAL_ADC_PollForConversion(&hadc1, 100))
+        {
+            // Conversion failed; restart the ADC
+            HAL_ADC_Stop(&hadc1);
+            HAL_Delay(10);
+            HAL_ADC_Start(&hadc1);
+            HAL_Delay(10);
+        }
+        uint32_t temp = HAL_ADC_GetValue(&hadc1);
+        temp_entropy = (temp_entropy << 2) | (temp & 0b00000011);       // use least 2 bits
+        HAL_ADC_Stop(&hadc1);
+        HAL_Delay(10); // Ensure readings from temperature sensor are uncorrelated
+    }
+
+    return temp_entropy;
+}
 
 static int
 hydro_random_init(void)
